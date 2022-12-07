5dcee1b3bb336107b0f4885dbab0a42ab461be29
jobin <jobinjose.in@gmail.com>
2022-12-07 22:50:44 +0530

    User product

--- src/main/java/com/example/ShoppingCartApp/ShoppingCartAppApplication.java
+++ src/main/java/com/example/ShoppingCartApp/ShoppingCartAppApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShoppingCartAppApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingCartAppApplication.class, args);
 	}
