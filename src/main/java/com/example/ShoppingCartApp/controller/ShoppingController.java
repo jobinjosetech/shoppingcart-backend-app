1d963695b3922bb9c22e388d0c884134fc41c4af
jobin <jobinjose.in@gmail.com>
2022-12-07 21:12:30 +0530

    userlogin

--- src/main/java/com/example/ShoppingCartApp/controller/ShoppingController.java
+++ src/main/java/com/example/ShoppingCartApp/controller/ShoppingController.java
@@ -1,6 +1,7 @@
 package com.example.ShoppingCartApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class ShoppingController {
     @GetMapping("/")
     public String HomePage(){
         return "Welcome to Shopping Cart API";
+    }
+
+    @PostMapping(path = "/userlogin", consumes = "application/json", produces = "application/json")
+    public String UserLogin(){
+        return "userlogin";
     }
 }
---
+++ src/main/java/com/example/ShoppingCartApp/dao/UserDao.java
@@ -0,0 +1,5 @@
+package com.example.ShoppingCartApp.dao;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserDao extends CrudRepository<> {
+}
